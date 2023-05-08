208f000a3642491f3ed0dcc1c35d48a22a9cce74
GabrielCode15 <jorgemagalhaes22.15@gmail.com>
2023-05-08 12:21:45 +0100

    .

---
+++ Ex.024.js
@@ -0,0 +1,13 @@
+function myFunction(){
+
+    let x = document.querySelector("#meusLinks")
+
+    if(x.style.display === "block"){
+
+        x.style.display = "none";
+
+    }else{
+        
+        x.style.display = "block"
+    }
+}
